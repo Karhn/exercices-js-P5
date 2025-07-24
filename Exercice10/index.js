937f2fb2ff0a486bc042ec2de9909a9d25521d3b
Gaston Dubois <drkarhn@gmail.com>
2025-07-24 15:20:22 +0200

    Exercice10

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,11 @@
 //votre code ici
 
+function printNumbers(n) {
+    const arr = [];
+    for (let i = 1; i <= n; i++) {
+        arr.push(i);
+    }
+    return arr.join(" ");
+}
+
 export default printNumbers
